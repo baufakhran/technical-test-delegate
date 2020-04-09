209452eae0e548f53b37dfff320defc79e95e9fc
baufakhran <baufakhran@gmail.com>
2020-04-09 21:03:46 +0700

    adjust in minimum screen size

--- components/CardNews.js
+++ components/CardNews.js
@@ -5,13 +5,9 @@ export default function CardNews(props) {
     <Card className="mt-2">
       <Card.Body className="my-card-body">
         <div>
-          <img
-            style={{ height: '150px', width: '250px' }}
-            src={props.urlImage}
-            className="my-card-img"
-          />
+          <img src={props.urlImage} className="my-card-img" />
         </div>
-        <div className="ml-4">
+        <div className="my-card-news">
           <Card.Title>{props.title}</Card.Title>
           <small className="text-muted">{props.description}</small>
         </div>
